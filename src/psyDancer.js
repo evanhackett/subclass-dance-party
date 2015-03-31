1b4e9aff020ea9e1d1c58dda26be1edddd68ef08
evanhackett <evanwhackett@gmail.com>
2015-03-30 17:27:59 -0700

    add psyDancers

---
+++ src/psyDancer.js
@@ -0,0 +1,29 @@
+var PsyDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  
+};
+
+PsyDancer.prototype = Object.create(Dancer.prototype);
+PsyDancer.prototype.constructor = PsyDancer;
+PsyDancer.prototype.step = function(){
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this);
+  
+  this.$node.toggle();
+
+  var randRed = Math.ceil(Math.random() * 255);
+  var randGreen = Math.ceil(Math.random() * 255);
+  var randBlue = Math.ceil(Math.random() * 255);
+
+
+  var randWidth = Math.ceil(Math.random() * 50);
+  var randHeight = Math.ceil(Math.random() * 50);
+
+  var styles = {
+    'border-color' : "rgb("+ randRed + ',' + randGreen + ',' + randBlue + ')',
+    width: randWidth+"px",
+    height: randHeight+"px"
+  };
+
+  this.$node.css(styles);
+};
