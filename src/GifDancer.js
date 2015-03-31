a4b167c8a9e87c0059e9c51eed955ff9a343cdd5
evanhackett <evanwhackett@gmail.com>
2015-03-30 17:03:54 -0700

    converted to pseudo classical, added a GifDancer

---
+++ src/GifDancer.js
@@ -0,0 +1,20 @@
+var GifDancer = function(top, left, timeBetweenSteps, gif){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="gifdancer" ><img style="height:50px" src="'+gif+'"></span>');
+  this.setPosition(top, left);
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+GifDancer.prototype = Object.create(Dancer.prototype);
+GifDancer.prototype.constructor = GifDancer;
+/*GifDancer.prototype.step = function(){
+    // call the old version of step at the beginning of any call to this new version of step
+    Dancer.prototype.step.call(this);
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    //this.$node.toggle();
+  };
+  */
+  
